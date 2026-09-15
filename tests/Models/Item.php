c0b9735b3d696c06facc27c3442e73e517d49980
eribys <erikas.bystautas@tutotoons.com>
2026-09-15 08:45:23 +0000

    [tests] update factory naming

--- tests/Models/Item.php
+++ tests/Models/Item.php
@@ -5,13 +5,15 @@ declare(strict_types=1);
 namespace Tests\Models;
 
 use ByErikas\EloquentQueryCache\Traits\CanCacheQueries;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Attributes\UseFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Tests\Database\Factories\ItemFactory;
 
+#[UseFactory(ItemFactory::class)]
 class Item extends Model
 {
-	use CanCacheQueries, SoftDeletes, HasFactory;
+	use CanCacheQueries, SoftDeletes;
 
 	protected $fillable = [
 		"keyword",
